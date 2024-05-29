ca568a1a63439d22907cbd38eaa2170b838d6982
DarrenChooo <darrenchoojh88@gmail.com>
2024-05-29 23:30:31 +0800

    Started contact me page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
@@ -5,8 +7,46 @@ import { FaTelegramPlane } from "react-icons/fa";
 import { MdArrowOutward } from "react-icons/md";
 import Skills from "../../components/skill";
 import Academic from "../../components/academic";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { MessageCircleIcon } from "lucide-react";
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "",
+  }),
+  email: z.string().min(2, {
+    message: "",
+  }),
+  enquiry: z.string().min(2, {
+    message: "",
+  }),
+});
 
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      enquiry: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
     <div className="px-16 py-8">
       <div className="flex items-center">
@@ -61,7 +101,7 @@ export default function Home() {
             <p className="text-2xl absolute -ml-10 -mt-8 -rotate-12 text-lightblue font-title">
               About Me
             </p>
-            <p className="text-7xl font-title pe-3 ">Introduction</p>
+            <p className="text-7xl font-title pe-3">Introduction</p>
             <div className="flex">
               <hr className="w-7 h-1 mt-3 bg-lightblue border-0 rounded"></hr>
               <p className="text-lg px-4 text-wrap basis-10/12">
@@ -79,6 +119,76 @@ export default function Home() {
         <div className="basis-5/12">
           <Skills></Skills>
         </div>
+      </div>
+      <div className="flex flex-col items-center space-y-6">
+        <p className="text-7xl font-title ">Contact Me</p>
+        <p className="text-xl text-wrap text-center">
+          Feel free to contact me any time. I will get back to you as soon as I
+          can!
+        </p>
+        <Image
+          src="/images/contact.png"
+          alt="Picture of Contact Me"
+          width={230}
+          height={230}
+          className="-ml-3"
+        />
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-1/3 flex flex-col space-y-6"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="name"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="enquiry"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Message</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="name"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </div>
     </div>
   );
